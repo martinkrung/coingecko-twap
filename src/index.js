3ff99ca8c59da9de67a0e8831cc1bbf9ff62f9de
Martin Krung <mk@cryptonative.ch>
2025-04-09 12:46:04 +0200

    real twap by claude, not avarage

--- src/index.js
+++ src/index.js
@@ -21,9 +21,34 @@ let coinId = argv.id;
 let days = argv.days;
 
 (async () => {
-    let url = `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=usd&days=${days}&interval=hourly`
+    let url = `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=usd&days=${days}`
     let response = await axios.get(url)
-    let twap = _(response.data.prices)
-        .meanBy(p => p[1])
-    console.log(twap)
+    
+    // Calculate TWAP - Time Weighted Average Price
+    const prices = response.data.prices;
+    let twap = 0;
+    let totalTimeWeight = 0;
+    
+    for (let i = 0; i < prices.length - 1; i++) {
+        const currentPrice = prices[i][1];
+        const currentTimestamp = prices[i][0];
+        const nextTimestamp = prices[i + 1][0];
+        const timeWeight = nextTimestamp - currentTimestamp;
+        
+        twap += currentPrice * timeWeight;
+        totalTimeWeight += timeWeight;
+    }
+    
+    // Handle the last price point if needed (using same weight as previous interval)
+    if (prices.length > 0) {
+        const lastPrice = prices[prices.length - 1][1];
+        if (prices.length > 1) {
+            const lastInterval = prices[prices.length - 1][0] - prices[prices.length - 2][0];
+            twap += lastPrice * lastInterval;
+            totalTimeWeight += lastInterval;
+        }
+    }
+    
+    twap = twap / totalTimeWeight;
+    console.log(twap);
 })()
